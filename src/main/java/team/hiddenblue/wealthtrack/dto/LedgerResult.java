0955e932983bfe4a86d1620dabcd1909a850ac59
BaoGaiYu <870340488@qq.com>
2023-12-30 17:36:43 +0800

    添加JsonNaming注解，启用蛇形命名转换。 e.g Java类中isPublic的属性以JSON的形式发送到前端时会自动转换成is_public

--- src/main/java/team/hiddenblue/wealthtrack/dto/LedgerResult.java
+++ src/main/java/team/hiddenblue/wealthtrack/dto/LedgerResult.java
@@ -1,10 +1,15 @@
 package team.hiddenblue.wealthtrack.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class LedgerResult {
     /**
      * 账本id
